c874cde5467703aa9a69b0d0068bc39bd95a7953
kolia <200drexc@gmail.com>
2020-09-20 18:14:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+struct Person
+{
+    public string firstName;
+    public string LastName;
+    public int age;
+
+    public Person(String firstName, string LastName, int age)
+    {
+        this.firstName = firstName;
+        this.LastName = LastName;
+        this.age = age;
+    }
+
+    public override string ToString()
+    {
+        return firstName + " " + LastName + ", age" + age;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person("Tony", "Allen", 20);
+        Console.WriteLine(person);
+    }
+}
+
